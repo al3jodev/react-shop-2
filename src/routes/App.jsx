0e11be6e2ec6ae43753894349d4a481e09ef30b3
Alejandro Jaramillo <ajaramillo@technisys.com>
2022-10-20 13:34:51 -0500

    [UPDATE] Configuracion para visualizar la pagina de Login v4

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -22,8 +22,8 @@ const App = () => {
 			<HashRouter>
 				<Layout>
 					<Switch>
-						<Route exact path="/react-shop-2" component={Home} />
-						<Route exact path="/react-shop-2/login" component={Login} />
+						<Route exact path="/" component={Home} />
+						<Route exact path="/login" component={Login} />
 						<Route exact path="/password-recovery" component={PasswordRecovery} />
 						<Route exact path="/send-email" component={SendEmail} />
 						<Route exact path="/new-password" component={NewPassword} />
